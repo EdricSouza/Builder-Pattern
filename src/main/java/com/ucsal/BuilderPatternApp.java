93bf89ac524b4d18aba1ba762f4701169a9dd46a
unknown <andre.liborio2005@gmail.com>
2026-05-28 17:33:29 -0300

    fix:trade instance to use ReflectiveBuilder class

--- src/main/java/com/ucsal/BuilderPatternApp.java
+++ src/main/java/com/ucsal/BuilderPatternApp.java
@@ -1,50 +1,24 @@
 package com.ucsal;
 
+import java.sql.Ref;
 import java.util.Map;
 
 public class BuilderPatternApp {
 
     /*
-     * O problema principal dessa abordagem é que o construtor fica grande, cheio de null,
-     * e a criação do objeto se torna pouco clara.
+     * Aplicando o padrao Reflection
      * */
     static void main() {
 
-        HttpRequestData requestGet = new HttpRequestData(
-                "GET",
-                "https://api.ucsal.com/alunos",
-                null,
-                Map.of("status", "ativo"),
-                null,
-                null,
-                "Bearer token123"
-        );
+
+        HttpRequestData requestGet = new ReflectiveBuilder<>(HttpRequestData.class)
+                .with("method","GET")
+                .with("url","https://api.ucsal.com/alunos")
+                .with("queryParams",Map.of("status", "ativo"))
+                .with("authorizationToken","Bearer token123").build();
 
         System.out.println(requestGet);
 
-        HttpRequestData requestPost = new HttpRequestData(
-                "POST",
-                "https://api.ucsal.com/alunos",
-                Map.of("Accept", "application/json"),
-                null,
-                "{\"nome\":\"João\"}",
-                "application/json",
-                "Bearer token123"
-        );
-
-        System.out.println(requestPost);
-
-        HttpRequestData requestPut = new HttpRequestData(
-                "PUT",
-                "https://api.ucsal.com/alunos/1",
-                Map.of("Accept", "application/json"),
-                null,
-                "{\"nome\":\"João Atualizado\"}",
-                "application/json",
-                "Bearer token123"
-        );
-
-        System.out.println(requestPut);
 
     }
 
