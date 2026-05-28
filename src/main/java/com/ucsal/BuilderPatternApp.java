1a9839e20cf385a7af90f8f7054127913d1c260d
Edric Souza <edric.souza@CARTORIO.LOCAL>
2026-05-28 15:57:11 -0300

    feat: Implementação dos outros métodos http.

--- src/main/java/com/ucsal/BuilderPatternApp.java
+++ src/main/java/com/ucsal/BuilderPatternApp.java
@@ -11,14 +11,37 @@ public class BuilderPatternApp {
      * O problema principal dessa abordagem é que o construtor fica grande, cheio de null,
      * e a criação do objeto se torna pouco clara.
      * */
-    static void main() {
+        static void main() {
 
-        HttpRequest requestGet = new HttpRequestBuilder()
-                .Method("GET")
-                .Url("https://api.ucsal.com/alunos")
-                .QueryParameters(Map.of("status", "ativo"))
-                .AuthorizationToken("Bearer token123")
-                .build();
-    }
+                HttpRequest requestGet = new HttpRequestBuilder()
+                        .Method("GET")
+                        .Url("https://api.ucsal.com/alunos")
+                        .QueryParameters(Map.of("status", "ativo"))
+                        .AuthorizationToken("Bearer token123")
+                        .build();
 
+                        System.out.println(requestGet);
+
+                HttpRequest requestPost = new HttpRequestBuilder()
+                        .Method("POST")
+                        .Url("https://api.ucsal.com/alunos")
+                        .Header(Map.of("Accept", "application/json"))
+                        .QueryParameters(Map.of("status", "ativo"))
+                        .Body( "{\"nome\":\"João\"}")
+                        .AuthorizationToken("Bearer token123")
+                        .ContentType("application/json")
+                        .AuthorizationToken("Bearer token123")
+                        .build();
+
+                        System.out.println(requestPost);
+
+                HttpRequest requestPut = new HttpRequestBuilder()
+                        .Method("GET")
+                        .Url("https://api.ucsal.com/alunos")
+                        .QueryParameters(Map.of("status", "ativo"))
+                        .AuthorizationToken("Bearer token123")
+                        .build();
+
+                        System.out.println(requestPut);
+        }
 }
